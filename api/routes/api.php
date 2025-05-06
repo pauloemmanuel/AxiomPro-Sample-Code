660ec98c0813f73404c8cd7df235cae4a0b07674
Paulo Emmanuel Ferreira de Oliveira <pauloemmanuelferreira@gmail.com>
2025-05-06 20:33:37 -0300

    feat: add /ping health-check endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,16 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get("/ping", function () {
+    return response()->json([
+        "message" => "pong",
+        "datetime" => Carbon\Carbon::now()
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
